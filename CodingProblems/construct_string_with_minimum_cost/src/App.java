848e8c0b35d87eca0263f825501cf4c7058e185a
BANGBANGROX <lakshya0809bang@gmail.com>
2024-07-12 20:14:41 +0530

    Fixing min cost to construct string

--- CodingProblems/construct_string_with_minimum_cost/src/App.java
+++ CodingProblems/construct_string_with_minimum_cost/src/App.java
@@ -3,122 +3,133 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 class Solution {
-    private int[] power;
-    private int[] inversePower;
+    private long[] power;
+    private long[] inversePower;
     private final int MOD = (int) 1e9 + 7;
-    private final int PRIME_NUMBER = 31;
-    private final int SIZE = (int) 1e5;
+    private final int BASE = 31;
 
-    Solution() {
-        computePower();
-        computeInversePower();
-        // print(power);
-        // print(inversePower);
+    private void computePowerAndInversePower(int maxSize) {
+        power = new long[maxSize + 1];
+        inversePower = new long[maxSize + 1];
+
+        power[0] = 1;
+        inversePower[0] = 1;
+
+        for (int i = 1; i <= maxSize; ++i) {
+            power[i] = (power[i - 1] * BASE) % MOD;
+            inversePower[i] = calculateBinExponent(power[i], MOD - 2);
+        }
     }
 
-    private int calculatePower(int a, int b) {
-        int result = 1;
+    private long calculateBinExponent(long a, long b) {
+        long result = 1;
 
         while (b > 0) {
             if ((b & 1) > 0) {
-                result = (int) ((1L * result * a) % MOD);
+                result = (result * a) % MOD;
                 --b;
             }
-            a = (int) ((1L * a * a) % MOD);
+            a = (a * a) % MOD;
             b >>= 1;
         }
 
         return result;
     }
 
-    private void computePower() {
-        power = new int[SIZE];
-
-        power[0] = 1;
-
-        for (int i = 1; i < SIZE; ++i) {
-            power[i] = (int) ((1L * power[i - 1] * PRIME_NUMBER) % MOD);
-        }
-    }
-
-    private void computeInversePower() {
-        inversePower = new int[SIZE];
-
-        inversePower[0] = 1;
-
-        for (int i = 1; i < SIZE; ++i) {
-            inversePower[i] = calculatePower(power[i], MOD - 2);
-        }
-    }
-
-    private int[] calculateHash(String s) {
+    private long[] computeHash(String s) {
         int n = s.length();
-        int[] hash = new int[n];
+        long[] hash = new long[n];
 
         for (int i = 0; i < n; ++i) {
-            int current = (int) ((1L * power[i] * (s.charAt(i) - 'a' + 1)) % MOD);
-            hash[i] = ((i > 0 ? hash[i - 1] : 0) + current) % MOD;
+            hash[i] = (power[i] * (s.charAt(i) - 'a' + 1)) % MOD;
+            if (i > 0) {
+                hash[i] = (hash[i] + hash[i - 1]) % MOD;
+            }
         }
 
         return hash;
     }
 
-    private int getSubstringHash(int[] hash, int left, int right) {
-        int val = (hash[right] - (left > 0 ? hash[left - 1] : 0) + MOD) % MOD;
+    private long getSubstringHash(int left, int right, long[] hash) {
+        long hashValue = (hash[right] - (left > 0 ? hash[left - 1] : 0) + MOD) % MOD;
 
-        return (int) ((1L * val * inversePower[left]) % MOD);
+        return (hashValue * inversePower[left]) % MOD;
     }
 
     public int minimumCost(String target, String[] words, int[] costs) {
         int n = target.length();
-        int[] dp = new int[n];
-        int[] hash = calculateHash(target);
-        Map<Integer, Integer> hashToMinCostMap = new HashMap<>();
-        ArrayList<Integer> uniqueLengths = new ArrayList<>();
 
-        Arrays.fill(dp, Integer.MAX_VALUE);
+        computePowerAndInversePower(n);
+
+        long[] targetHash = computeHash(target);
+        List<Integer> uniqueLengths = new ArrayList<>();
+        Map<Long, Long> hashValueToCostMap = new HashMap<>();
 
         for (int i = 0; i < words.length; ++i) {
             String word = words[i];
-            int cost = costs[i];
             int len = word.length();
-            int currentWordHash = calculateHash(word)[len - 1];
-            hashToMinCostMap.put(currentWordHash,
-                    Math.min(hashToMinCostMap.getOrDefault(currentWordHash, Integer.MAX_VALUE), cost));
+            long wordHashValue = computeHash(word)[len - 1];
+            Long hashCost = hashValueToCostMap.get(wordHashValue);
+            if (hashCost == null) {
+                hashValueToCostMap.put(wordHashValue, 1L * costs[i]);
+            } else {
+                hashValueToCostMap.put(wordHashValue, Math.min(1L * costs[i], hashCost));
+            }
             uniqueLengths.add(len);
         }
-
-        System.out.println(hashToMinCostMap);
 
         Collections.sort(uniqueLengths);
 
         uniqueLengths = new ArrayList<>(new HashSet<>(uniqueLengths));
 
+        long[] dp = new long[n];
+
+        Arrays.fill(dp, Long.MAX_VALUE);
+
         for (int i = 0; i < n; ++i) {
             for (int len : uniqueLengths) {
-                if (i >= len - 1) {
-                    int substringHash = getSubstringHash(hash, i - len + 1, i);
-                    System.out.println(substringHash);
-                    Integer minHashCost = hashToMinCostMap.get(substringHash);
-                    if (minHashCost != null) {
-                        int prevValue = (i >= len ? dp[i - len] : 0);
-                        if (prevValue != Integer.MAX_VALUE) {
-                            dp[i] = Math.min(dp[i], prevValue + minHashCost);
+                if (i - len + 1 >= 0) {
+                    long substringHash = getSubstringHash(i - len + 1, i, targetHash);
+                    Long minCost = hashValueToCostMap.get(substringHash);
+                    if (minCost != null) {
+                        long previousValue = (i >= len ? dp[i - len] : 0);
+                        if (previousValue != Long.MAX_VALUE) {
+                            dp[i] = Math.min(dp[i], previousValue + minCost);
                         }
                     }
                 }
             }
         }
 
-        return dp[n - 1] == Integer.MAX_VALUE ? -1 : dp[n - 1];
+        return dp[n - 1] == Long.MAX_VALUE ? -1 : (int) dp[n - 1];
     }
 }
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        int testCases = scanner.nextInt();
+
+        while (testCases-- > 0) {
+            String target = scanner.next();
+            int totalWords = scanner.nextInt();
+            String[] words = new String[totalWords];
+            for (int i = 0; i < totalWords; ++i) {
+                words[i] = scanner.next();
+            }
+            int[] costs = new int[totalWords];
+            for (int i = 0; i < totalWords; ++i) {
+                costs[i] = scanner.nextInt();
+            }
+
+            System.out.println(new Solution().minimumCost(target, words, costs));
+        }
+
+        scanner.close();
     }
 }
